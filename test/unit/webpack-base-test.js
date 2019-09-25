82f0be0960449577d88fc239b1c4dfb69e01365a
yuyongkun4519 <yuyongkun4519@qq.com>
2019-09-25 16:31:58 +0800

    test3

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,6 +4,7 @@ describe('webapck.base.js test case', () => {
     it('entry', () => {
         console.log('------------test------------')
         console.log(baseConfig.entry.index)
+        console.log(baseConfig.entry.index.indexOf('travis-test/test/smoke/template/src/index/index.js') > -1)
         assert.equal(baseConfig.entry.index.indexOf('travis-test/test/smoke/template/src/index/index.js') > -1, true)
         assert.equal(baseConfig.entry.search.indexOf('travis-test/test/smoke/template/src/search/index.js') > -1, true)
     })
