d494364f341cfacf00b8104e05c45f368298a8db
yuyongkun4519 <yuyongkun4519@qq.com>
2019-09-25 15:05:14 +0800

    修改webpack-base-test.js

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 describe('webapck.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base');
     it('entry', () => {
-        assert.equal(baseConfig.entry.index.indexOf('travis-test/test/smoke/template/src/index/index.j') > -1, true)
-        assert.equal(baseConfig.entry.search.indexOf('travis-test/test/smoke/template/src/search/index.j') > -1, true)
+        assert.equal(baseConfig.entry.index.indexOf('travis-test/test/smoke/template/src/index/index.js') > -1, true)
+        assert.equal(baseConfig.entry.search.indexOf('travis-test/test/smoke/template/src/search/index.js') > -1, true)
     })
 })
