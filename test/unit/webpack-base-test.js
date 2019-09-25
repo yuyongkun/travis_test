e3d80c0dabbc65f22ef0f5bb37db708982f796f8
yuyongkun4519 <yuyongkun4519@qq.com>
2019-09-25 16:43:34 +0800

    travis中划线自动转下划线优化

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -2,10 +2,7 @@ const assert = require('assert');
 describe('webapck.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base');
     it('entry', () => {
-        console.log('------------test------------')
-        console.log(baseConfig.entry.index)
-        console.log(baseConfig.entry.index.indexOf('travis-test/test/smoke/template/src/index/index.js') > -1)
-        assert.equal(baseConfig.entry.index.indexOf('travis-test/test/smoke/template/src/index/index.js') > -1, true)
-        assert.equal(baseConfig.entry.search.indexOf('travis-test/test/smoke/template/src/search/index.js') > -1, true)
+        assert.equal(baseConfig.entry.index.indexOf('/test/smoke/template/src/index/index.js') > -1, true)
+        assert.equal(baseConfig.entry.search.indexOf('/test/smoke/template/src/search/index.js') > -1, true)
     })
 })
